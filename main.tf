5637d62cc468a0f6109d8abea6fbb7e770e67760
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-01-12 17:51:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_kms_key" "my_kms_key" {
             "Sid": "Enable IAM User Permissions",
             "Effect": "Allow",
             "Principal": {
-                "AWS": ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+                "AWS": ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:${var.user_arn_root}"]
             },
             "Action": "kms:*",
             "Resource": "*"
