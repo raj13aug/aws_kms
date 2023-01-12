b6c50d355a1e185f4a76d5353989833edeccdd4b
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-01-12 13:37:34 +0530

    first file

---
+++ dynamodb.tf
@@ -0,0 +1,18 @@
+resource "aws_dynamodb_table" "metadata" {
+  name             = join("-", ["octagon-ObjectMetadata", var.environment])
+  billing_mode     = "PAY_PER_REQUEST"
+  hash_key         = "id"
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = aws_kms_key.my_kms_key.arn
+  }
+
+}
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "user_arn" {
+  default = "arn:aws:iam::932999788441:user/Terraform"
+}
+
+variable "key_spec" {
+  default = "SYMMETRIC_DEFAULT"
+}
+
+variable "enabled" {
+  default = true
+}
+
+output "key_id" {
+  value = aws_kms_key.my_kms_key.key_id
+}
+
+output "key_arn" {
+  value = aws_kms_key.my_kms_key.arn
+}
